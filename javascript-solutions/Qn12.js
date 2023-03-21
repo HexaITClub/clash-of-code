85794d7d450fcb9bb910d98db6a134160a86366d
Subedi Bibek <pingsubedibibek@gmail.com>
2023-03-22 00:05:16 +0545

    Solved Question no. 12 using JavaScrip )

--- javascript-solutions/Qn12.js
+++ javascript-solutions/Qn12.js
@@ -8,3 +8,6 @@ function calculateProduct(num1, num2) {
   
   console.log(calculateProduct(2, 2)); // Output: 4
   console.log(calculateProduct(4, 2)); // Output: 8
+
+
+  // Solution by Subedi Bibek
