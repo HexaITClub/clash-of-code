49fa182d444be406b28af790d8971a78193a7dc8
Subedi Bibek <pingsubedibibek@gmail.com>
2023-03-21 23:16:53 +0545

    Solved Question no. 15 using JavaScrip (node)

---
+++ javascript-solutions/Qn15.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("BCA");
+    } else if (i % 3 === 0) {
+      console.log("Patan");
+    } else if (i % 5 === 0) {
+      console.log("Campus");
+    } else {
+      console.log(i);
+    }
+  }
+
+  // solution by subedibibek
