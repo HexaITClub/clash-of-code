869ade49af2ddd40a161e9a352455c27648d24ef
rigel-star <boyzdod@gmail.com>
2023-03-21 22:46:24 +0545

    bug fixed for in between arrays

--- python-solutions/qn8.py
+++ python-solutions/qn8.py
@@ -2,16 +2,21 @@
 
 def valid_nep_name(name):
     name = name.strip()
+    name_arr = name.split(" ")
+    if len(name_arr) == 1: return -1
+
     spaces = 0
-    for c in name:
-        if c == ' ':
+    for c in name_arr:
+        if c == ' ' or c == '':
+            continue
+        else:
             spaces += 1
     if spaces == 0:
         return -1
-    return spaces + 1
+    return spaces
 
 def main():
-    print(valid_nep_name("Ramesh Poudel"))
+    print(valid_nep_name("Ramesh   Poudel"))
     print(valid_nep_name("RameshPoudel"))
     print(valid_nep_name("Ramesh Poudel  "))
     print(valid_nep_name(" Ramesh Poudel Prasad"))
